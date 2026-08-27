15aa8ce67622ce997f0ff9bbc2ad93b2df72dc33
ssromeo <122471342+ssromeo@users.noreply.github.com>
2026-08-27 15:49:37 +0200

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        s = s.lower()
+        resultat = ""
+        for c in s:
+            if c.isalnum():
+                resultat+=c
+        compteur = 0
+        for i in range(0,len(resultat)):
+            if resultat[i] != resultat[len(resultat) -1 - compteur]:
+                return False
+            compteur +=1
+        return True
+        
---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-11.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+
+        sortedArray = list(sorted(set(nums)))
+        print(sortedArray)
+        
+        output = 0
+        temp = 0
+        for i in range(0,len(sortedArray)-1):
+            #faire le if pour pass le for si c le meme cas. 
+            if sortedArray[i+1] == sortedArray[i] +1:
+                temp += 1
+              
+            else:               
+                temp = 0
+            if temp > output:
+                output = temp            
+        
+        if not nums:
+            return 0
+        return output+1
+        
